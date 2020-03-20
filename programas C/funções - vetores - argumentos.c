ac17f90f7dfec9fd6f0fdd5436903e1d91c226fc
delvecch1o <61480650+delvecch1o@users.noreply.github.com>
2020-03-20 20:15:26 -0300

    aula 36 ->  funções recebem argumentos

---
+++ programas C/funções - vetores - argumentos.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+   
+    int vetor [10] = {6,4,1,9,8,0,5,7,3,2};
+    int i;
+    void ordemCrescente (int vetor [], int n); // não eh necessario declarar a qauntidade de vetores [10]
+    
+    ordemCrescente(vetor, 10);
+    
+	for (i = 0; i < 10; i++ ){
+    printf("%d\n", vetor[i]);
+	}
+	
+    system("pause");
+    return 0;
+}
+
+void ordemCrescente (int vetor [], int n){
+	
+	int i, j, temporaria;
+	
+	for (i = 0; i < n; i++){
+		for (j = i + 1; j < n; j++) {
+			if (vetor[i] > vetor [j]) {
+				temporaria = vetor [i];
+				vetor [i] = vetor [j];
+				vetor [j] = temporaria;
+			}
+			
+		}
+	}	
+}
