8f9cfbf56f315e9a50e346f46820c598a36b66ac
delvecch1o <61480650+delvecch1o@users.noreply.github.com>
2020-03-31 17:26:40 -0300

    aula 58 -> tabela ASCII

---
+++ programas C/tabela ASCII.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int x = 130;
+	
+	printf("%c", x); // ira printar é
+	
+	if (x < 'a')
+	
+	
+	
+	
+	
+	
+	
+    getchar();
+    return 0;
+}
