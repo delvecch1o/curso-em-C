76f67b4e6de95e9c265b2ca07faa87f3fd6643f4
delvecch1o <61480650+delvecch1o@users.noreply.github.com>
2020-03-31 17:15:37 -0300

    aula 57 -> Sequencia de Escape, \a,\t,\r...

---
+++ programas C/Sequencia de Escape.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	printf("Bom diiiiiiiiiia vizinho!");
+	
+    getchar();
+    return 0;
+}
+
+/*
+
+\a Alarme sonoro do sistema
+\b Move  o cursor uma posição à esquerda
+\n Pula para a proxima linha
+\t Tecla  TAB
+\r Volta para o inicio da linha
+\0 Caractere nulo ou zero geralmente estabelecido como fim de string
+\" Corresponde ao caractere "
+\' Corresponde ao caractere '
+
+*/
