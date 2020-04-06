132c92cfc4baeeaaf6b470ae7586c937724a0b05
delvecch1o <61480650+delvecch1o@users.noreply.github.com>
2020-04-05 22:39:46 -0300

    Rename manipulacao de arquivos txt  Escrever dados.c to manipulacao de arquivos txt  ESCREVER "W" dados.c

--- programas C/manipulacao de arquivos txt  ESCREVER "W" dados.c
+++ programas C/manipulacao de arquivos txt  ESCREVER "W" dados.c
@@ -4,7 +4,7 @@
 int main(){
 	
 	FILE *file;
-	file = fopen("E:\\Projetos UniFCV\\Projeto C\\aula 81 manipulação de arquivos\\teste.txt", "w");
+	file = fopen("E:\\Projetos UniFCV\\Projeto C\\aula 81 manipulaÃ§Ã£o de arquivos\\teste.txt", "w");
 	fprintf(file,"Hoje eh Domingo,dia 5 de abril de 2020 as 22:30\n");
 	fclose(file);
 	
