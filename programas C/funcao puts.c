b810a23c4103149a97f41946ccf8b9aa83eb0f9f
delvecch1o <61480650+delvecch1o@users.noreply.github.com>
2020-04-06 20:53:01 -0300

    aula 92 -> função puts

---
+++ programas C/funcao puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	char x[] = "ola caro leitor";
+    puts(x);
+	   	
+//	puts ("jose dirceu"); // essa função ela quebra a linha automatico
+//	puts ("jose dirceu");
+	
+    system("pause");
+    return 0;
+}
